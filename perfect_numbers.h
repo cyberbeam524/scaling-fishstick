4ae3807f42aecda5f82d4fbc3d8a01a798c55aec
jdolittle524 <jdolittle524@gmail.com>
2023-08-26 19:08:00 +0800

    inventory operations abstracted out as Application & Presentation layers

--- perfect_numbers.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-namespace perfect_numbers {
-enum classification : int {
-    deficient = -1,
-    perfect   = 0,
-    abundant  = +1,
-};
-auto classify(int n) -> classification;
-} // namespace perfect_numbers
